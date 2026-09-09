5a27f4cf4c7885427daa684eef6061b8ee760934
benkennedy98 <bmhk2011@gmail.com>
2026-09-09 16:13:56 -0600

    chore: convert to next js

---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image"
+
+export default function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Image
+          src="/logo.svg"
+          alt="logo"
+          className="App-logo"
+          width={40}
+          height={40}
+        />
+        <p>
+          Edit <code>app/page.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://nextjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Next.js
+        </a>
+      </header>
+    </div>
+  )
+}
